2adf807188d52cf3b4d881e0897d855d77bba799
abgr <andi.berger@yandex.com>
2019-04-03 21:54:39 +0200

    auto-adjust scrollbar opacity, reworking lxqt themes, more

--- kvantum/Trellium.source/make_theme.sh
+++ kvantum/Trellium.source/make_theme.sh
@@ -27,8 +27,31 @@ while read key val alpha; do
 	alpha[$key]=$alpha
 done < "colors.$colorscheme"
 
-for file in *.svg *.kvconfig; do
+
+# Calculate opacity of scrollbars/sliders, which are rendered on top of (also translucent) widgets, assuming an average background value (0-255)
+avg_bg_brightness=85
+bg_brightness=$(bc <<< "($((0x$(echo ${colors_theme[bg]} | cut -b2-3))) + \
+	                 $((0x$(echo ${colors_theme[bg]} | cut -b4-5))) + \
+	                 $((0x$(echo ${colors_theme[bg]} | cut -b6-7)))) / 3")
+bg_pressed_brightness=$(bc <<< "($((0x$(echo ${colors_theme[bg-pressed]} | cut -b2-3))) + \
+	                         $((0x$(echo ${colors_theme[bg-pressed]} | cut -b4-5))) + \
+		                 $((0x$(echo ${colors_theme[bg-pressed]} | cut -b6-7)))) / 3")
+bg_alpha=${alpha[bg]}
+bg_pressed_alpha=${alpha[bg-pressed]}
+scrollbar_alpha=$(bc -l <<< "($bg_pressed_alpha*($avg_bg_brightness*($bg_alpha-1)+$bg_pressed_brightness-$bg_brightness*$bg_alpha))/($bg_alpha*($avg_bg_brightness*($bg_pressed_alpha-1)*($bg_alpha-1)+$bg_pressed_brightness*$bg_pressed_alpha+$bg_brightness*(-1*$bg_pressed_alpha*$bg_alpha+$bg_alpha-1)))")
+min=$(echo 1 $scrollbar_alpha | tr ' ' '\n' | sort -n | head -1)
+max=$(echo 0 $min             | tr ' ' '\n' | sort -n | tail -1)
+scrollbar_alpha=$max
+
+
+for i in ${!colors_source[@]}; do
+	hexalpha=$(printf %02x $(bc <<< "(255 * ${alpha[$i]} + 0.5) / 1"))
+	sed -i "s/${colors_source[$i]}/${colors_theme[$i]}$hexalpha/" *.kvconfig
+done
+
+for file in *.svg; do
 	sed -i '/#ff00ff/s/fill-opacity:1/fill-opacity:0/' $file
+	sed -i "/#808080/s/opacity:0.751/opacity:$scrollbar_alpha/" $file
 	
 	for i in ${!colors_source[@]}; do
 		sed -i "/${colors_source[$i]}/s/fill-opacity:1/fill-opacity:${alpha[$i]}/g" $file
